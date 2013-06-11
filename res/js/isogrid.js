8f5920572c51080cb6f0e49e2fe48b7070b36759
Cezary Wojcik <cezarywojcik92@gmail.com>
2013-06-10 18:36:43 -0700

    Added some comments to isogrid.js

--- res/js/isogrid.js
+++ res/js/isogrid.js
@@ -1,17 +1,29 @@
 jQuery(function($){
 	$.fn.isogrid = function(options) {
+		// settings
 		var settings = {
 			rows: 10,
 			cols: 10,
 			tilesize: 32
 		};
+		// custom settings
 		$.extend(settings, options);
 		
+		// creating the canvas
 		var el = $(this);
-		el.html('');
-		el.append('<canvas id="tilelayer"></canvas>');
-		el.append('<canvas id="uilayer"></canvas>');
+		el.html('<canvas id="tilelayer"></canvas>');
 		var tileLayer = $('#tilelayer');
-		var uiLayer = $('#uilayer');
+
+		// object will contain methods to interact with the grid
+		var object = {
+			update: function(data) {
+				// update
+			},
+			translate: function(vector) {
+				// translate
+			}
+		}
+
+		return object;
 	}
 });
