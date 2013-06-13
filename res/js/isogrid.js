86c89ddc4043fa1f60f1662cf5dd545bda116a6a
Cezary Wojcik <cezarywojcik92@gmail.com>
2013-06-12 20:39:24 -0700

    made test file and test grid to develop the toIso function

--- res/js/isogrid.js
+++ res/js/isogrid.js
@@ -1,3 +1,43 @@
+// "classes"
+function point2D(x, y) {
+	var result = {};
+	result.x = x;
+	result.y = y;
+	return result;
+}
+
+// functions
+function toIso(point, camera) {
+	var isox = point.x;
+	var isoy = point.y;
+	return point2D(isox, isoy);
+}
+
+function generateGrid(cols, rows, tileSize, canvasId) {
+	var canvas = document.getElementById(canvasId);
+	var context = canvas.getContext('2d');
+	var width = cols*tileSize;
+	var height = rows*tileSize;
+	var camera = {};
+	context.beginPath();
+	for (i = 0; i <= cols; i++) {
+		from = toIso(point2D(i*tileSize, 0), camera);
+		to = toIso(point2D(i*tileSize, height), camera);
+		context.moveTo(from.x, from.y);
+		context.lineTo(to.x, to.y);
+	}
+	for (i = 0; i <= rows; i++) {
+		from = toIso(point2D(0, i*tileSize), camera);
+		to = toIso(point2D(width, i*tileSize), camera);
+		context.moveTo(from.x, from.y);
+		context.lineTo(to.x, to.y);
+	}
+	context.closePath();
+	context.stroke();
+}
+
+
+// $.fn.isogrid implementation
 jQuery(function($){
 	$.fn.isogrid = function(options) {
 		// settings
