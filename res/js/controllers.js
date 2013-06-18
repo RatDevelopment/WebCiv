85ec8fb1bfcb289d7e147d6cf41b86fdb42b2d9c
Cezary Wojcik <cezarywojcik92@gmail.com>
2013-06-18 10:37:50 -0700

    Made messages sync using angular as well

--- res/js/controllers.js
+++ res/js/controllers.js
@@ -1,7 +1,13 @@
-function LobbyController($scope, socket) {
+function WCController($scope, socket) {
 	$scope.lobbies = [];
+	$scope.messages = [];
 
 	socket.on('lobbies', function (data) {
 		$scope.lobbies = data.lobbies;
 	});
+
+	socket.on('message', function (data) {
+		$scope.messages.push(data.message);
+	});
+
 }
