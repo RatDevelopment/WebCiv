dda8d4367128b8e22fc392fa98bd70ee0d4f2ec2
Chris Rees <serneum@gmail.com>
2013-06-20 23:58:02 -0400

    Changed tabs to spaces

--- res/js/generator.js
+++ res/js/generator.js
@@ -1,96 +1,96 @@
 // "classes"
 function point2D(x, y) {
-	var result = {};
-	result.x = x;
-	result.y = y;
-	return result;
+  var result = {};
+  result.x = x;
+  result.y = y;
+  return result;
 }
 
 function tile(x, y, type, neighbors) {
-	return {
-		x: x,
-		y: y,
-		type: type,
-		neighbors: neighbors
-	};
+  return {
+    x: x,
+    y: y,
+    type: type,
+    neighbors: neighbors
+  };
 }
 
 // get tile function for generator
 function getTile(x, y) {
-	var tx = x;
-	var ty = y;
-	while (tx < 0) {
-		tx += this.cols;
-	}
-	while (tx > this.cols-1) {
-		tx -= this.cols;
-	}
-	var result = this.tiles[tx][ty];
-	if (typeof result === "undefined") {
-		return tile(x, y, "blank", {});
-	} else {
-		return tile(x, y, result.type, result.neighbors);
-	}
+  var tx = x;
+  var ty = y;
+  while (tx < 0) {
+    tx += this.cols;
+  }
+  while (tx > this.cols-1) {
+    tx -= this.cols;
+  }
+  var result = this.tiles[tx][ty];
+  if (typeof result === "undefined") {
+    return tile(x, y, "blank", {});
+  } else {
+    return tile(x, y, result.type, result.neighbors);
+  }
 }
 
 function getMap(rows, cols) {
-	if (rows % 2 !== 0 || cols % 2 !== 0) {
-		throw "The number of rows and columns must be even.";
-	}
-	var map = {
-		rows: rows,
-		cols: cols,
-		tiles: [],
-		getTile: getTile
-	};
-	for (var i = 0; i < cols; i++) {
-		map.tiles[i] = [];
-		for (var j = 0; j < rows; j++) {
-			var type = Math.random() < 0.7 ? "ground" : "water";
-			// neighbors will be a list of point2D elements
-			var neighbors = {};
-			if (j !== 0) {
-				// top left
-				if (i === 0) {
-					neighbors.topleft = point2D(cols-1, j-1);
-				} else {
-					neighbors.topleft = point2D(i-1, j-1);
-				}
-				// top right
-				if (i === cols-1) {
-					neighbors.topright = point2D(0, j-1);
-				} else {
-					neighbors.topright = point2D(i+1, j-1);
-				}
-			}
-			if (j !== rows-1) {
-				// bottom left
-				if (i === 0) {
-					neighbors.bottomleft = point2D(cols-1, j+1);
-				} else {
-					neighbors.bottomleft = point2D(i-1, j+1);
-				}
-				// bottom right
-				if (i === cols-1) {
-					neighbors.bottomright = point2D(0, j+1);
-				} else {
-					neighbors.bottomright = point2D(i+1, j+1);
-				}
-			}
-			// left
-			if (j === 0) {
-				neighbors.left = point2D(cols-1, j);
-			} else {
-				neighbors.left = point2D(i-1, j);
-			}
-			// right
-			if (j === cols-1) {
-				neighbors.right = point2D(0, j);
-			} else {
-				neighbors.right = point2D(i+1, j);
-			}
-			map.tiles[i][j] = tile(i, j, type, neighbors);
-		}
-	}
-	return map;
+  if (rows % 2 !== 0 || cols % 2 !== 0) {
+    throw "The number of rows and columns must be even.";
+  }
+  var map = {
+    rows: rows,
+    cols: cols,
+    tiles: [],
+    getTile: getTile
+  };
+  for (var i = 0; i < cols; i++) {
+    map.tiles[i] = [];
+    for (var j = 0; j < rows; j++) {
+      var type = Math.random() < 0.7 ? "ground" : "water";
+      // neighbors will be a list of point2D elements
+      var neighbors = {};
+      if (j !== 0) {
+        // top left
+        if (i === 0) {
+          neighbors.topleft = point2D(cols-1, j-1);
+        } else {
+          neighbors.topleft = point2D(i-1, j-1);
+        }
+        // top right
+        if (i === cols-1) {
+          neighbors.topright = point2D(0, j-1);
+        } else {
+          neighbors.topright = point2D(i+1, j-1);
+        }
+      }
+      if (j !== rows-1) {
+        // bottom left
+        if (i === 0) {
+          neighbors.bottomleft = point2D(cols-1, j+1);
+        } else {
+          neighbors.bottomleft = point2D(i-1, j+1);
+        }
+        // bottom right
+        if (i === cols-1) {
+          neighbors.bottomright = point2D(0, j+1);
+        } else {
+          neighbors.bottomright = point2D(i+1, j+1);
+        }
+      }
+      // left
+      if (j === 0) {
+        neighbors.left = point2D(cols-1, j);
+      } else {
+        neighbors.left = point2D(i-1, j);
+      }
+      // right
+      if (j === cols-1) {
+        neighbors.right = point2D(0, j);
+      } else {
+        neighbors.right = point2D(i+1, j);
+      }
+      map.tiles[i][j] = tile(i, j, type, neighbors);
+    }
+  }
+  return map;
 }
--- res/js/isogrid.js
+++ res/js/isogrid.js
@@ -1,142 +1,142 @@
 // "classes"
 
 function sprite(src) {
-	var img = new Image();
-	img.src = 'res/img/' + src;
-	return img;
+  var img = new Image();
+  img.src = 'res/img/' + src;
+  return img;
 }
 
 // sprites
 var sprites = {
-	ground: sprite('ground.png'),
-	water: sprite('water.png'),
-	blank: sprite('blank.png')
+  ground: sprite('ground.png'),
+  water: sprite('water.png'),
+  blank: sprite('blank.png')
 };
 
 // functions
 function translate(point, translation) {
-	return point2D(point.x+translation.x, point.y+translation.y);
+  return point2D(point.x+translation.x, point.y+translation.y);
 }
 
 function verticalDilute(point, dilution) {
-	return point2D(point.x, point.y/dilution);
+  return point2D(point.x, point.y/dilution);
 }
 
 function toIso(point, camera) {
-	var translatedPoint = translate(point, camera);
-	var isox = Math.floor(translatedPoint.x);
-	var isoy = Math.floor(translatedPoint.y);
-	return point2D(isox, isoy);
+  var translatedPoint = translate(point, camera);
+  var isox = Math.floor(translatedPoint.x);
+  var isoy = Math.floor(translatedPoint.y);
+  return point2D(isox, isoy);
 }
 
 function drawSprite(tile, tileWidth, yspace, camera, context) {
-	var xoffset = Math.abs(tile.y) % 2 === 1 ? tileWidth/2 : 0;
-	var spritePoint = toIso(point2D(xoffset + tile.x*tileWidth,
-		tile.y*yspace), camera);
-	context.drawImage(sprites[tile.type], spritePoint.x, spritePoint.y);
+  var xoffset = Math.abs(tile.y) % 2 === 1 ? tileWidth/2 : 0;
+  var spritePoint = toIso(point2D(xoffset + tile.x*tileWidth,
+    tile.y*yspace), camera);
+  context.drawImage(sprites[tile.type], spritePoint.x, spritePoint.y);
 }
 
 function drawGrid(map, tileWidth, tileHeight, canvasId, camera) {
-	var docwidth = $(window).width();
-	var docheight = $(window).height()-5;
-	var canvas = document.getElementById(canvasId);
-	$('#' + canvasId).attr("width", docwidth);
-	$('#' + canvasId).attr("height", docheight);
-	var context = canvas.getContext('2d');
-	var yspace = Math.floor(3*tileHeight/4);
-	var camx = Math.floor(-1*camera.x/tileWidth);
-	var camy = Math.floor(-1*camera.y/yspace);
-	for (var i = -1; i <= Math.ceil(docwidth/tileWidth); i++) {
-		for (var j = -1; j <= Math.ceil(docheight/yspace); j++) {
-			var tile = map.getTile(i+camx, j+camy);
-				drawSprite(tile, tileWidth, yspace, camera, context);
-		}
-	}
+  var docwidth = $(window).width();
+  var docheight = $(window).height()-5;
+  var canvas = document.getElementById(canvasId);
+  $('#' + canvasId).attr("width", docwidth);
+  $('#' + canvasId).attr("height", docheight);
+  var context = canvas.getContext('2d');
+  var yspace = Math.floor(3*tileHeight/4);
+  var camx = Math.floor(-1*camera.x/tileWidth);
+  var camy = Math.floor(-1*camera.y/yspace);
+  for (var i = -1; i <= Math.ceil(docwidth/tileWidth); i++) {
+    for (var j = -1; j <= Math.ceil(docheight/yspace); j++) {
+      var tile = map.getTile(i+camx, j+camy);
+        drawSprite(tile, tileWidth, yspace, camera, context);
+    }
+  }
 }
 
 // $.fn.isogrid implementation
 jQuery(function($){
-	$.fn.isogrid = function(options) {
-		// settings
-		var settings = {
-			map: {},
-			tileWidth: 128,
-			tileHeight: 106
-		};
+  $.fn.isogrid = function(options) {
+    // settings
+    var settings = {
+      map: {},
+      tileWidth: 128,
+      tileHeight: 106
+    };
 
-		// custom settings
-		$.extend(settings, options);
+    // custom settings
+    $.extend(settings, options);
 
-		// make camera, center it
-		var camera = point2D(-1*settings.map.cols*settings.tileWidth/2,
-			-1*settings.map.rows*settings.tileHeight/2);
-		camera = point2D(0,0);
+    // make camera, center it
+    var camera = point2D(-1*settings.map.cols*settings.tileWidth/2,
+      -1*settings.map.rows*settings.tileHeight/2);
+    camera = point2D(0,0);
 
-		// initialize grid
-		function init() {
-			// generate grid
-			object.redraw();
-		}
+    // initialize grid
+    function init() {
+      // generate grid
+      object.redraw();
+    }
 
-		// object will contain methods to interact with the grid
-		var object = {
-			update: function(data) {
-				// update
-			},
-			translate: function(vector) {
-				// translate
-			},
-			redraw: function() {
-				drawGrid(settings.map, settings.tileWidth, settings.tileHeight,
-					elid, camera);
-			}
-		};
+    // object will contain methods to interact with the grid
+    var object = {
+      update: function(data) {
+        // update
+      },
+      translate: function(vector) {
+        // translate
+      },
+      redraw: function() {
+        drawGrid(settings.map, settings.tileWidth, settings.tileHeight,
+          elid, camera);
+      }
+    };
 
-		// creating the canvas
-		var el = $(this);
-		var elid = $(this).attr('id');
-		el.html('<canvas id="game"></canvas>');
-		var tileLayer = $('#game');
+    // creating the canvas
+    var el = $(this);
+    var elid = $(this).attr('id');
+    el.html('<canvas id="game"></canvas>');
+    var tileLayer = $('#game');
 
-		// mouse pan handler
-		var mouseIsDown = false;
-		var mousedownx = 0;
-		var mousedowny = 0;
-		var mousedowncam = {};
-		$(document).mousedown(function(e) {
-			mouseIsDown = true;
-			el.css('cursor', 'move');
-			mousedownx = e.pageX;
-			mousedowny = e.pageY;
-			mousedowncam = {
-				x: camera.x,
-				y: camera.y
-			};
-		});
-		$(document).mouseup(function() {
-			mouseIsDown = false;
-			el.css('cursor', 'default');
-		});
-		$(document).mousemove(function(e) {
-			if (mouseIsDown) {
-				var mousex = e.pageX;
-				var mousey = e.pageY;
-				var newx = mousedowncam.x + mousex - mousedownx;
-				var newy = mousedowncam.y + mousey - mousedowny;
-				camera.x = newx;
-				camera.y = newy;
-				object.redraw();
-			}
-		});
+    // mouse pan handler
+    var mouseIsDown = false;
+    var mousedownx = 0;
+    var mousedowny = 0;
+    var mousedowncam = {};
+    $(document).mousedown(function(e) {
+      mouseIsDown = true;
+      el.css('cursor', 'move');
+      mousedownx = e.pageX;
+      mousedowny = e.pageY;
+      mousedowncam = {
+        x: camera.x,
+        y: camera.y
+      };
+    });
+    $(document).mouseup(function() {
+      mouseIsDown = false;
+      el.css('cursor', 'default');
+    });
+    $(document).mousemove(function(e) {
+      if (mouseIsDown) {
+        var mousex = e.pageX;
+        var mousey = e.pageY;
+        var newx = mousedowncam.x + mousex - mousedownx;
+        var newy = mousedowncam.y + mousey - mousedowny;
+        camera.x = newx;
+        camera.y = newy;
+        object.redraw();
+      }
+    });
 
-		// resize handler
-		$(window).resize(function() {
-			object.redraw();
-		});
+    // resize handler
+    $(window).resize(function() {
+      object.redraw();
+    });
 
-		// init on load image
-		sprites.ground.onload = init;
+    // init on load image
+    sprites.ground.onload = init;
 
-		return object;
-	};
+    return object;
+  };
 });
