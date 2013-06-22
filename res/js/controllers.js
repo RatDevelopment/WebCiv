626ba4adcdf7e53f7e1e9344eedc857bf57bbfea
Chris Rees <serneum@gmail.com>
2013-06-22 06:23:05 -0400

    Converted index to use Jade templating engine. Added partials for each 'view'

--- res/js/controllers.js
+++ res/js/controllers.js
@@ -19,6 +19,46 @@ controllers.WCController = function ($scope, socket) {
     $scope.messages = [];
     $scope.lobby = $.localStorage('lobby');
   });
+
+  $scope.submitName = function() {
+    var name = $('#nameInput').val();
+    $('#loginContent').hide();
+    $('#listingContent').show();
+    $('#newLobby').click(function() {
+      socket.emit('lobby:new', {});
+    });
+    socket.emit('name chosen', {
+      name: name
+    });
+
+    $.localStorage('lobby', '');
+    $.localStorage('username', name);
+    return false;
+  };
+
+  $scope.sendMessage = function() {
+    var name = $.localStorage('username');
+    var lobby = getLobby();
+    var message = $('#messageField').val();
+
+    if (message) {
+      socket.emit('message', {
+        name: name,
+        lobby: lobby,
+        message: name + ': ' + message
+      });
+      $('#messageField').val('');
+    }
+    return false;
+  };
+
+  var getLobby = function() {
+    var lobby = $.localStorage('lobby');
+    if (!lobby) {
+      lobby = '';
+    }
+    return lobby;
+  };
 };
 
 app.controller(controllers);
